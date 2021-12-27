15b91e0f06abb5900b2c69f1cf6e951ea8233658
kjin64 <kjin64@gatech.edu>
2021-12-27 00:24:36 -0600

    Create 937 Reorder Data in Log Files.java

---
+++ 937 Reorder Data in Log Files.java
@@ -0,0 +1,34 @@
+
+//937. Reorder Data in Log Files
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+		int n = logs.length;
+		
+		Arrays.sort(logs, new Comparator<String>(){
+			public int compare(String log1, String log2){
+				//index 2 means split the string to 2 parts
+				String[] a = log1.split(' ', 2);
+				String[] b = log2.split(' ', 2);
+				//check contents is digit or not
+				boolean a1 = Character.isDigit(a[1].charAt(0), a2 = Character.isDigit(b[1].charAt(0));
+				//both contents are not digit
+				if(!a1 && !a2){
+					//check contents
+					int tmp = a[1].compareTo(b[1]);
+					if(tmp != 0) return tmp;
+					//if the same contents check identifier
+					return a[0].compareTo(b[0]);
+				}
+				//log1 is letter, log2 is digit
+				if(!a1){
+					return -1;
+				}
+				if(!a2){
+					return 1;
+				}
+				return 0;
+			}
+		});
+		return logs;
+	}
+}
