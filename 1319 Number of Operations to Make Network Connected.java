3892301750844c03879b8c22e57e70ab68645f8c
apejkj <apejkj@gmail.com>
2022-01-30 17:14:10 -0600

    Update 1319 Number of Operations to Make Network Connected.java

--- 1319 Number of Operations to Make Network Connected.java
+++ 1319 Number of Operations to Make Network Connected.java
@@ -26,3 +26,35 @@ class Solution {
 		return n - 1;
 	}
 }
+
+//dfs
+class Solution {
+	List<Integer>[] l;
+	boolean[] used;
+    public int makeConnected(int n, int[][] con) {
+		if(con.length < n - 1) return -1;
+		l = new List[n];
+		for(int[] c : con){
+			l[c[0]].add(c[1]);
+			l[c[1]].add(c[0]);
+		}
+		int ans = 0;
+		used = new boolean[n];
+		for(int i = 0; i < n; i ++){
+			if(!used[i]){
+				dfs(i);
+				ans ++;
+			}
+		}
+		return ans - 1;
+	}
+	
+	public void dfs(int x){
+		used[x] = true;
+		for(int v : l[x]){
+			if(!used[v]){
+				dfs(v);
+			}
+		}
+	}
+}
