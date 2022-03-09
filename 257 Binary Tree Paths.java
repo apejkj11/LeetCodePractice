65401d6dfcd6cfaa0adc913d6c1194fccfb47e9f
apejkj <apejkj@gmail.com>
2022-03-08 23:08:02 -0600

    Update 257 Binary Tree Paths.java

--- 257 Binary Tree Paths.java
+++ 257 Binary Tree Paths.java
@@ -1,6 +1,7 @@
 
 //257. Binary Tree Paths
 
+//dfs backtracking
 class Solution {
     List<String> ans = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
@@ -23,4 +24,57 @@ class Solution {
 	   dfs(root.right);
 	   path.remove(path.size() - 1);
    }
+}
+
+//dfs no backtracking
+
+class Solution {
+    List<String> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+		path += root.val;
+		dfs(root, path);
+		return ans;
+   }
+   
+	public void dfs(TreeNode root, String path){
+		if(root.left == null && root.right == null){
+		   ans.add(path);
+		}
+		if(root.left != null){
+			dfs(root.left, path + "->" + root.left.val);
+		}
+		if(root.right != null){
+			dfs(root.right, path + "->" + root.right.val);
+		}
+	}
+}
+
+//bfs
+
+class Solution {
+	public List<String> binaryTreePaths(TreeNode root) {
+		List<String> ans = new ArrayList<>();
+		if(root == null) return ans;
+		Queue<String> qs = new LinkedList<>();
+		Queue<TreeNode> qt = new LinkedList<>();
+		qt.offer(root);
+		qs.offer(String.valueOf(root.val));
+		while(!qt.isEmpty()){
+			TreeNode cur = qt.poll();
+			String p = qs.poll();
+			if(cur.left == null && cur.right == null){
+				ans.add(p);
+			}
+			if(cur.left != null){
+				qs.offer(p + "->" + cur.left.val);
+				qt.offer(cur.left);
+			}
+			if(cur.right != null){
+				qs.offer(p + "->" + cur.right.val);
+				at.offer(cur.right);
+			}
+		}
+		return ans;
+	}
 }
