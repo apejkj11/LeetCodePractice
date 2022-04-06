c7a75c40d4b00a14a41bc1cb0cecd2167790e5e2
apejkj <apejkj@gmail.com>
2022-04-06 16:54:55 -0500

    Update 242 Valid Anagram.java

--- 242 Valid Anagram.java
+++ 242 Valid Anagram.java
@@ -1,6 +1,6 @@
 
 //242. Valid Anagram
-
+//sort: Time-O(nlogn)
 class Solution {
     public boolean isAnagram(String s, String t) {
 		if(s.length() != t.length()) return false;
@@ -8,5 +8,21 @@ class Solution {
 		Arrays.sort(ss);
 		Arrays.sort(tt);
 		return Arrays.equals(ss, tt);
+	}
+}
+//Time O(n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+		int[] a = new int[26];
+        for(char c : s.toCharArray()){
+            a[c - 'a']++;
+        }
+        for(char c: t.toCharArray()){
+            a[c - 'a']--;
+        }
+        for(int x : a){
+            if(x != 0) return false;
+        }
+        return true;
 	}
 }
